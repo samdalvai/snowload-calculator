e6ff295c1f8fe9c2abb671efe5d9b85e0b661ea1
samdalvai <samuel.dalvai@gmail.com>
2022-09-19 16:14:37 +0200

    Remove clients api

--- src/main/java/org/snowstop/repository/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package org.snowstop.repository;
-
-import org.snowstop.model.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-
-}

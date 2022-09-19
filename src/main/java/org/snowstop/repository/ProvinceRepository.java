363950b8b2d587344600b98cf7fa3d4b2662402d
samdalvai <samuel.dalvai@gmail.com>
2022-09-19 16:57:53 +0200

    Add repository methods for provinces

--- src/main/java/org/snowstop/repository/ProvinceRepository.java
+++ src/main/java/org/snowstop/repository/ProvinceRepository.java
@@ -5,6 +5,13 @@ import org.snowstop.model.CityId;
 import org.snowstop.model.Province;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProvinceRepository  extends JpaRepository<Province, String> {
+    List<Province> findProvinceByShorthand(String shorthand);
+    List<Province> findProvinceByName(String name);
+    List<Province> findProvinceByNameContains(String name);
+    List<Province> findProvinceByZone(String zone);
+    List<Province> findProvinceByLoadBetween(Double lower, Double upper);
 
 }

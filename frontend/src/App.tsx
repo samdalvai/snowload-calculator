630aa5fec4ffa073ea73a0057b444c429cbdbb90
samdalvai <samuel.dalvai@gmail.com>
2022-09-20 15:33:40 +0200

    Make API hook general

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
+import {useApiGet} from "./functions/useApiHook";
 
 function App() {
+    const { data } = useApiGet('/cities/namecontains/Bolzano');
 
+    console.log(data);
 
     return (
         <div className="container">

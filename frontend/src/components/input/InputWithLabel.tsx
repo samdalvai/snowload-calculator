1463bb988e936a653447e1f1744019e8f97cc9e7
samdalvai <samuel.dalvai@gmail.com>
2022-09-27 18:22:01 +0200

    Reformat code

--- frontend/src/components/input/InputWithLabel.tsx
+++ frontend/src/components/input/InputWithLabel.tsx
@@ -1,17 +1,19 @@
 import {useState} from "react";
 import {StringCallBack} from "../../functions/callbacks";
 
-export const InputWithLabel = ({label, placeHolder, units, initialValue, onChange}: {label: string, placeHolder: string, units: string, initialValue: string, onChange: StringCallBack
+export const InputWithLabel = ({label, placeHolder, units, initialValue, onChange}: {
+    label: string, placeHolder: string, units: string, initialValue: string, onChange: StringCallBack
 }) => {
     const [value, setValue] = useState<string>(initialValue)
 
     return (
         <div className="input-group shadow-sm rounded">
             <label className="input-group-text" style={{minWidth: "45%"}}>{label}</label>
-            <input type="text" className="form-control" value={value} onChange={event => {
-                setValue(event.target.value)
-                onChange(event.target.value)
-            }} placeholder={placeHolder} required/>
+            <input type="text" className="form-control" value={value}
+                   onChange={event => {
+                       setValue(event.target.value)
+                       onChange(event.target.value)
+                   }} placeholder={placeHolder} required/>
             <label className="input-group-text" style={{minWidth: "12.5%"}}>{units}</label>
         </div>
     )
@@ -19,7 +21,8 @@ export const InputWithLabel = ({label, placeHolder, units, initialValue, onChang
 
 
 // Currently not used by the application, to be used in case we want to be able to change unit of measure, e.g. (° or %)
-export const InputWithChangeableRightLabel = ({label, placeHolder, units}: {label: string, placeHolder: string, units: string[]
+export const InputWithChangeableRightLabel = ({label, placeHolder, units}: {
+    label: string, placeHolder: string, units: string[]
 }) => {
     const [unitsIndex, setUnitsIndex] = useState<number>(0);
     const [unitsLabel, setUnitsLabel] = useState<string>(units[0]);

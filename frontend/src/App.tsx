b4cae1f7acad60f7a3f370cb8d102cdda4084671
samdalvai <samuel.dalvai@gmail.com>
2022-09-27 15:52:35 +0200

    Create inoput with changeable units

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import {CitiesSearch} from "./components/selection/cities/CitiesSearch";
 import {CitiesSelector} from "./components/selection/cities/CitiesSelector";
-import {NumberInputWithLabel} from "./components/input/NumberInputWithLabel";
+import {InputWithChangeableRightLabel, InputWithLabel} from "./components/input/InputWithLabel";
 import {CheckBoxWithDescription} from "./components/input/CheckBoxWithDescription";
 
 function App() {
@@ -12,15 +12,15 @@ function App() {
 
             <div className="row">
                 <div className="col-md-6 pt-3">
-                    <NumberInputWithLabel label={'Steepness (α)'} placeHolder={'Insert steepness...'}   units={'°'}/>
+                    <InputWithChangeableRightLabel label={'Steepness (α)'} placeHolder={'Insert steepness...'} units={['°','%']}/>
                 </div>
                 <div className="col-md-6 pt-3">
-                    <NumberInputWithLabel label={'Roof length (sl)'} placeHolder={'Insert roof length...'} units={'m'} />
+                    <InputWithLabel label={'Roof length (sl)'} placeHolder={'Insert roof length...'} units={'m'} />
                 </div>
             </div>
             <div className="row">
                 <div className="col-md-6 pt-3">
-                    <NumberInputWithLabel label={'Roof width (l)'} placeHolder={'Insert roof width...'}   units={'m'}/>
+                    <InputWithLabel label={'Roof width (l)'} placeHolder={'Insert roof width...'} units={'m'}/>
                 </div>
                 <div className="col-md-6 pt-3">
                     <CheckBoxWithDescription  placeHolder={'1.5 safety coefficient'}/>
---
+++ frontend/src/components/input/InputWithLabel.tsx
@@ -0,0 +1,38 @@
+import {useState} from "react";
+
+export const InputWithLabel = ({label, placeHolder, units}: {label: string, placeHolder: string, units: string
+}) => {
+    return (
+        <div className="input-group">
+            <label className="input-group-text" style={{minWidth: "35%"}}>{label}</label>
+            <input type="text" className="form-control" placeholder={placeHolder} required/>
+            <label className="input-group-text" style={{minWidth: "10%"}}>{units}</label>
+        </div>
+    )
+}
+
+export const InputWithChangeableRightLabel = ({label, placeHolder, units}: {label: string, placeHolder: string, units: string[]
+}) => {
+    const [unitsIndex, setUnitsIndex] = useState<number>(0);
+    const [unitsLabel, setUnitsLabel] = useState<string>(units[0]);
+    const [rightLabelStyle, setRightLaberStyle] = useState<any>({minWidth: "10%"})
+
+    const updateUnits = () => {
+        const newIndex = (unitsIndex + 1) % units.length;
+        setUnitsIndex(newIndex);
+        setUnitsLabel(units[newIndex]);
+    }
+
+    return (
+        <div className="input-group">
+            <label className="input-group-text" style={{minWidth: "35%"}}>{label}</label>
+            <input type="text" className="form-control" placeholder={placeHolder} required/>
+            <label className="input-group-text" style={rightLabelStyle}
+                   onClick={updateUnits}
+                   onMouseEnter={() => setRightLaberStyle({minWidth: "10%", backgroundColor: "lightblue"})}
+                   onMouseLeave={() => setRightLaberStyle({minWidth: "10%"})}
+            >
+                {unitsLabel}</label>
+        </div>
+    )
+}

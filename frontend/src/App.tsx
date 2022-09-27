d479568bb08688b9fd24932169754016c592dda9
samdalvai <samuel.dalvai@gmail.com>
2022-09-27 15:32:52 +0200

    Make inputs accept text

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,16 +12,15 @@ function App() {
 
             <div className="row">
                 <div className="col-md-6 pt-3">
-                    <NumberInputWithLabel label={'Steepness (α)'} min={0} max={90} placeHolder={'Insert steepness...'}   units={'°'}/>
+                    <NumberInputWithLabel label={'Steepness (α)'} placeHolder={'Insert steepness...'}   units={'°'}/>
                 </div>
                 <div className="col-md-6 pt-3">
-                    <NumberInputWithLabel label={'Roof length (sl)'} min={0} max={1000}
-                                          placeHolder={'Insert roof length...'} units={'m'} />
+                    <NumberInputWithLabel label={'Roof length (sl)'} placeHolder={'Insert roof length...'} units={'m'} />
                 </div>
             </div>
             <div className="row">
                 <div className="col-md-6 pt-3">
-                    <NumberInputWithLabel label={'Roof width (l)'} min={0} max={90} placeHolder={'Insert roof width...'}   units={'m'}/>
+                    <NumberInputWithLabel label={'Roof width (l)'} placeHolder={'Insert roof width...'}   units={'m'}/>
                 </div>
                 <div className="col-md-6 pt-3">
                     <CheckBoxWithDescription  placeHolder={'1.5 safety coefficient'}/>
--- frontend/src/components/input/NumberInputWithLabel.tsx
+++ frontend/src/components/input/NumberInputWithLabel.tsx
@@ -1,9 +1,9 @@
-export const NumberInputWithLabel = ({label, min, max, placeHolder, units}: {label: string, min: number, max: number, placeHolder: string, units: string
+export const NumberInputWithLabel = ({label, placeHolder, units}: {label: string, placeHolder: string, units: string
 }) => {
     return (
         <div className="input-group">
             <label className="input-group-text" style={{minWidth: "35%"}}>{label}</label>
-            <input type="number" min={min} max={max} className="form-control" placeholder={placeHolder} required/>
+            <input type="text" className="form-control" placeholder={placeHolder} required/>
             <label className="input-group-text" style={{minWidth: "10%"}}>{units}</label>
         </div>
     )

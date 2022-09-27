5c981adf2e5feca60f4f10bf0fd8e9fa3c704e4d
samdalvai <samuel.dalvai@gmail.com>
2022-09-27 21:37:14 +0200

    Use React bootstrap insetad of pure bootstrap for modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css';
 import {SnowloadCalculationForm} from "./components/input/snowload/SnowloadCalculationForm";
 import {MessageModal} from "./components/messages/MessageModal";
+import {useState} from "react";
 
 function App() {
+
     return (
         <div className="container p-3">
             <SnowloadCalculationForm/>
--- frontend/src/components/messages/MessageModal.tsx
+++ frontend/src/components/messages/MessageModal.tsx
@@ -1,24 +1,22 @@
 import {Button, Modal} from "react-bootstrap";
 import {useState} from "react";
 
-export const MessageModal = ({header, body}: {header: string, body: string}) => {
-    const [show, setShow] = useState<boolean>(true)
+export const MessageModal = ({header, body}: { header: string, body: string }) => {
+    const [show, setShow] = useState(true);
 
     return (
-        <div style={{display: "block"}} className={show ? "fade modal show" : "fade modal"} tabIndex={0}>
-            <div className="modal-dialog modal-dialog-centered">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <div className="modal-title h4">{header}</div>
-                        <button type="button" className="btn-close" aria-label="Close" onClick={() => setShow(false)}></button>
-                    </div>
-                    <div className="modal-body">{body}</div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" onClick={() => setShow(false)}>Close</button>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <Modal show={show} onHide={() => setShow(false)} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>{header}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>{body}</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShow(false)}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
-
-    )
+    );
 }

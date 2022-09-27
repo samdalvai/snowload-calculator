7859115b55a7b8065c40208ed1d4e8f2fc67f07d
samdalvai <samuel.dalvai@gmail.com>
2022-09-27 20:19:21 +0200

    Add state for roof measuser

--- frontend/src/components/input/roof/RoofMeasureInput.tsx
+++ frontend/src/components/input/roof/RoofMeasureInput.tsx
@@ -2,9 +2,8 @@ import {InputWithLabel} from "../InputWithLabel";
 import {CheckBoxWithDescription} from "../CheckBoxWithDescription";
 import {StringCallBack} from "../../../functions/callbacks";
 
-export const RoofMeasureInput = ({steepness, onSteepnessChange}: {steepness: string, onSteepnessChange: StringCallBack}) => {
-
-
+export const RoofMeasureInput = ({steepness, roofLength, roofWidth, onSteepnessChange, onRoofLengthChange, onRoofWidthChange}:
+                                     {steepness: string, roofLength: string, roofWidth: string, onSteepnessChange: StringCallBack, onRoofLengthChange: StringCallBack, onRoofWidthChange: StringCallBack}) => {
     return (
         <div>
             <div className="row">
@@ -12,12 +11,12 @@ export const RoofMeasureInput = ({steepness, onSteepnessChange}: {steepness: str
                     <InputWithLabel label={'Steepness (α)'} placeHolder={'Insert steepness...'} units={'°'} value={steepness} onChange={onSteepnessChange} />
                 </div>
                 <div className="col-md-6 pt-3">
-                    <InputWithLabel label={'Roof length (sl)'} placeHolder={'Insert roof length...'} units={'m'} value={''} onChange={() => null}/>
+                    <InputWithLabel label={'Roof length (sl)'} placeHolder={'Insert roof length...'} units={'m'} value={roofLength} onChange={onRoofLengthChange}/>
                 </div>
             </div>
             <div className="row">
                 <div className="col-md-6 pt-3">
-                    <InputWithLabel label={'Roof width (l)'} placeHolder={'Insert roof width...'} units={'m'} value={''} onChange={() => null}/>
+                    <InputWithLabel label={'Roof width (l)'} placeHolder={'Insert roof width...'} units={'m'} value={roofWidth} onChange={onRoofWidthChange}/>
                 </div>
                 <div className="col-md-6 pt-3">
                     <CheckBoxWithDescription placeHolder={'1.5 safety coefficient'}/>
--- frontend/src/components/input/snowload/SnowloadCalculationForm.tsx
+++ frontend/src/components/input/snowload/SnowloadCalculationForm.tsx
@@ -9,30 +9,38 @@ import {City} from "../../../functions/types";
 export const SnowloadCalculationForm = () => {
     const [selectedCity, setSelectedCity] = useState<City | null>(null)
     const [steepness, setSteepness] = useState<string>('')
+    const [roofLength, setRoofLength] = useState<string>('')
+    const [roofWidth, setRoofWidth] = useState<string>('')
 
     const resetInputs = () => {
         setSelectedCity(null)
         setSteepness('')
-
+        setRoofLength('')
+        setRoofWidth('')
     }
 
     return (
         <div>
-            <CitiesSelector selectedCity={selectedCity} onSelectedCity={setSelectedCity} />
-            <RoofMeasureInput steepness={steepness} onSteepnessChange={setSteepness} />
+            <CitiesSelector selectedCity={selectedCity} onSelectedCity={setSelectedCity}/>
+            <RoofMeasureInput steepness={steepness} roofLength={roofLength} roofWidth={roofWidth}
+                              onSteepnessChange={setSteepness} onRoofLengthChange={setRoofLength} onRoofWidthChange={setRoofWidth}/>
             <SnowloadButtonsGroup onCompute={() => null} onReset={resetInputs}/>
         </div>
     )
 }
 
-export const SnowloadButtonsGroup = ({onCompute, onReset}:{onCompute: Callback, onReset: Callback}) => {
+export const SnowloadButtonsGroup = ({onCompute, onReset}: { onCompute: Callback, onReset: Callback }) => {
     return (
         <div className="row">
             <div className="col-md-6 pt-3">
-                <button type="button" className="btn btn-primary shadow-sm rounded" style={{width: "100%"}} onClick={onCompute}><GearIcon size={20} /> Compute</button>
+                <button type="button" className="btn btn-primary shadow-sm rounded" style={{width: "100%"}}
+                        onClick={onCompute}><GearIcon size={20}/> Compute
+                </button>
             </div>
             <div className="col-md-6 pt-3">
-                <button type="button" className="btn btn-secondary shadow-sm rounded" style={{width: "100%"}} onClick={onReset}><TrashIcon size={20} /> Reset</button>
+                <button type="button" className="btn btn-secondary shadow-sm rounded" style={{width: "100%"}}
+                        onClick={onReset}><TrashIcon size={20}/> Reset
+                </button>
             </div>
         </div>
     )

2714317a888c0691b15635c9ebe287b5ca7f0fd1
samdalvai <samuel.dalvai@gmail.com>
2022-09-28 16:54:32 +0200

    Add state for inputs

--- frontend/src/components/input/cities/NewCityForm.tsx
+++ frontend/src/components/input/cities/NewCityForm.tsx
@@ -1,30 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import {InputWithLeftLabel, InputWithTwoLabels} from "../InputWithLabels";
 import {GearIcon, HomeIcon, TrashIcon} from "@primer/octicons-react";
 
 export const NewCityForm = () => {
+    const [zip, setZip] = useState<string>('')
+    const [name, setName] = useState<string>('')
+    const [province, setProvince] = useState<string>('')
+    const [altitude, setAltitude] = useState<string>('')
 
+    const resetInputs = () => {
+        setZip('')
+        setName('')
+        setProvince('')
+        setAltitude('')
+    }
 
     return (
         <div>
             <div className="row">
                 <div className="col-md-6 pt-3">
-                    <InputWithLeftLabel leftLabel={'ZIP'} placeHolder={''} value={''}
-                                        onChange={() => null} valid={true}/>
+                    <InputWithLeftLabel leftLabel={'ZIP'} placeHolder={''} value={zip}
+                                        onChange={setZip} valid={true}/>
                 </div>
                 <div className="col-md-6 pt-3">
-                    <InputWithLeftLabel leftLabel={'Name'} placeHolder={''} value={''}
-                                        onChange={() => null} valid={true}/>
+                    <InputWithLeftLabel leftLabel={'Name'} placeHolder={''} value={name}
+                                        onChange={setName} valid={true}/>
                 </div>
             </div>
             <div className="row">
                 <div className="col-md-6 pt-3">
-                    <InputWithLeftLabel leftLabel={'Province'} placeHolder={''} value={''}
-                                        onChange={() => null} valid={true}/>
+                    <InputWithLeftLabel leftLabel={'Province'} placeHolder={''} value={province}
+                                        onChange={setProvince} valid={true}/>
                 </div>
                 <div className="col-md-6 pt-3">
-                    <InputWithLeftLabel leftLabel={'Altitude'} placeHolder={''} value={''}
-                                        onChange={() => null} valid={true}/>
+                    <InputWithLeftLabel leftLabel={'Altitude'} placeHolder={''} value={altitude}
+                                        onChange={setAltitude} valid={true}/>
                 </div>
             </div>
             <div className="row">
@@ -35,7 +45,7 @@ export const NewCityForm = () => {
                 </div>
                 <div className="col-md-6 pt-3">
                     <button type="submit" className="btn btn-secondary shadow-sm rounded" style={{width: "100%"}}
-                            onClick={() => null}><TrashIcon size={20}/> Reset
+                            onClick={resetInputs}><TrashIcon size={20}/> Reset
                     </button>
                 </div>
             </div>

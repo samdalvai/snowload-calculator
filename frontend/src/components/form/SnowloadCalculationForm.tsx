99dc913ef15aafd477da827eeaab8589238bc0cf
samdalvai <samuel.dalvai@gmail.com>
2022-10-03 16:05:28 +0200

    Add terms of use and roof image

--- frontend/src/components/form/SnowloadCalculationForm.tsx
+++ frontend/src/components/form/SnowloadCalculationForm.tsx
@@ -1,12 +1,10 @@
 import {RoofMeasureInput} from "../input/roof/RoofMeasureInput";
 import {CitiesSelector} from "../selection/cities/CitiesSelector";
-import {GearIcon, TrashIcon} from "@primer/octicons-react";
 import {Callback, RoofDataCallback} from "../../functions/callbacks";
 import React, {useState} from "react";
 import {City} from "../../functions/types";
 import {isInputBetweenLowerAndUpperBound, isValidSteepness} from "../../functions/validation/stringValidation";
 import {StringToFloatNumber} from "../../functions/conversion/stringConversion";
-import {SnowFlakeIcon} from "../icon/SnowFlakeIcon";
 import {Alert} from "../alert/Alert";
 import {ComputeButton} from "../button/ComputeButton";
 import {ResetButton} from "../button/ResetButton";
@@ -82,7 +80,7 @@ export const SnowloadCalculationForm = ({onCompute}: { onCompute: RoofDataCallba
             <div className="card-body">
                 {
                     showAlert ? <Alert type={"danger"} message={'You have an error in your input, please retry...'}
-                                            onClose={() => setShowAlert(false)}/> : ""
+                                       onClose={() => setShowAlert(false)}/> : ""
                 }
                 <div onChange={() => setValidCityInput(true)}>
                     <CitiesSelector selectedCity={selectedCity} onSelectedCity={setSelectedCity}
@@ -110,6 +108,16 @@ export const SnowloadCalculationForm = ({onCompute}: { onCompute: RoofDataCallba
                                           resetInputs()
                                           resetInvalidInputs()
                                       }}/>
+                <div className="row">
+                    <div className="col-md-6 pt-3 image-container">
+                        <div className="img-fluid">
+                            <img src={"/img/roof.jpg"} className="rounded" alt="Roof"/>
+                        </div>
+                    </div>
+                    <div className="col-md-6 pt-3">
+                        <TermsOfUse/>
+                    </div>
+                </div>
             </div>
         </div>
     )
@@ -119,11 +127,32 @@ export const SnowloadButtonsGroup = ({onCompute, onReset}: { onCompute: Callback
     return (
         <div className="row">
             <div className="col-md-6 pt-3">
-                <ComputeButton onCompute={onCompute} />
+                <ComputeButton onCompute={onCompute}/>
             </div>
             <div className="col-md-6 pt-3">
-                <ResetButton  onReset={onReset}/>
+                <ResetButton onReset={onReset}/>
             </div>
+        </div>
+    )
+}
+
+export const TermsOfUse = () => {
+    return (
+        <div>
+            <h5><strong>Terms of use</strong></h5>
+            <p className={"font-12"}>
+                The information you provide will be automatically processed by our system in accordance with
+                DIN 1055-5 / EN1991 and the specifications of the ZVDH leaflet on installation parts.
+                Occurring special features such as projections on roofs, exposed locations, compliance with
+                special safety regulations and or structural features are not taken into account and must be
+                verified separately. This product generation applies in connection with the specific installation
+                instructions and the specifications of the ZVDH Merkblatt Einbauteile in the currently valid version.
+            </p>
+            <p className={"font-12"}>
+                For the highest possible stability of the snow protection system, an exact specification of the
+                requested data is required. The information is processed automatically by the system used and is not checked
+                individually for correctness. No liability can be accepted for incorrect data.
+            </p>
         </div>
     )
 }

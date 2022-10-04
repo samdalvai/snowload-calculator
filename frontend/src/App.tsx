62580f5d75648a0d53f6d548bd9fa3cd23890efe
samdalvai <samuel.dalvai@gmail.com>
2022-10-04 16:34:17 +0200

    Remove unused modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,10 @@
 import './App.css';
 import {SnowloadCalculationForm} from "./components/form/SnowloadCalculationForm";
-import {AddCityModal} from "./components/modal/Modal";
-import {useState} from "react";
 
 function App() {
-    const [show, setShow] = useState<boolean>(false)
-
     return (
         <div className="container p-3">
             <SnowloadCalculationForm onCompute={console.log}/>
-            <AddCityModal show={show} onHide={() => setShow(false)} />
         </div>
     );
 }

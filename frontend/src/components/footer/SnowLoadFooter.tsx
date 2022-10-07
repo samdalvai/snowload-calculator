709cb9616310719565372b49b43db18708e5bd45
samdalvai <samuel.dalvai@gmail.com>
2022-10-07 15:00:08 +0200

    Add example elements in footer

--- frontend/src/components/footer/SnowLoadFooter.tsx
+++ frontend/src/components/footer/SnowLoadFooter.tsx
@@ -1,15 +1,33 @@
 import React from "react";
+import {SnowFlakeIcon} from "../icon/SnowFlakeIcon";
 
 export const SnowLoadFooter = () => {
     return (
         <div>
             <div className="container-fluid shadow-sm top-border snowload-grey-background">
-                <footer className={"text-center p-3"} >
-                    <h1 className={"snowload-grey"} >
-                        <strong>
-                            Snowload Footer
-                        </strong>
-                    </h1>
+                <footer className={"p-3"}>
+                    <div className="row">
+                        <div className="col-12 col-md white text-shadow">
+                            <h5><SnowFlakeIcon size={22}/> <strong>Snowload Calculator</strong></h5>
+                            <small className="d-block mb-3 text-light">© 2022 Copyright</small>
+                        </div>
+                        <div className="col-6 col-md">
+                            <h5 className={"white text-shadow"}>Resources</h5>
+                            <ul className="list-unstyled text-small">
+                                <li className="mb-1"><a>Resource</a></li>
+                                <li className="mb-1"><a>Another resource</a></li>
+                                <li className="mb-1"><a>Final resource</a></li>
+                            </ul>
+                        </div>
+                        <div className="col-6 col-md">
+                            <h5 className={"white text-shadow"}>About</h5>
+                            <ul className="list-unstyled text-small">
+                                <li className="mb-1"><a>Team</a></li>
+                                <li className="mb-1"><a>Privacy</a></li>
+                                <li className="mb-1"><a>Terms</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </footer>
             </div>
         </div>

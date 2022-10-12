88bb13b49e586ea1bc9f2544139632e3e0d84fec
samdalvai <samuel.dalvai@gmail.com>
2022-10-12 15:12:30 +0200

    Highlight city suggestion on arrow up and down

--- frontend/src/components/selection/CitiesSuggestion.tsx
+++ frontend/src/components/selection/CitiesSuggestion.tsx
@@ -1,37 +1,47 @@
 import {City} from "../../functions/types";
 import {CityCallBack} from "../../functions/callbacks";
 import {getCityString} from "../../functions/search/searchCity";
-import React from "react";
+import React, {useState} from "react";
 import {useKeyBoardPress} from "../../functions/hooks/useKeyBoardPress";
 
 export const CitiesSuggestionList = ({cities, keyword, onSelectCity}: { cities: City[], keyword: string, onSelectCity: CityCallBack }) => {
-    useKeyBoardPress(["ArrowUp"], () => {
-        console.log("Arrow up!!")
+    const [selectedIndex, setSelectedIndex] = useState<number>(-1)
 
+    React.useEffect(() => {
+        setSelectedIndex(-1)
+    }, [cities])
+
+    useKeyBoardPress(["ArrowUp"], () => {
+        console.log("Arrow up!! ", selectedIndex)
+        if (selectedIndex > 0)
+            setSelectedIndex(selectedIndex - 1)
     })
 
     useKeyBoardPress(["ArrowDown"], () => {
-        console.log("Arrow down!!")
+        console.log("Arrow down!! ", selectedIndex)
+        if (selectedIndex < cities.length - 1)
+            setSelectedIndex(selectedIndex + 1)
     })
 
     return (
         <div className="list-group shadow-sm rounded">
             {
                 cities.length > 0 ?
-                    cities.map((city: City) => <CitySuggestion
+                    cities.map((city: City, index) => <CitySuggestion
                         city={city}
                         keyword={keyword}
                         onSelectCity={onSelectCity}
-                        key={city.zip+city.name}/>) : ""
+                        key={city.zip+city.name} selected={index === selectedIndex}/>) : ""
             }
         </div>
     )
 }
 
-export const CitySuggestion = ({city, keyword, onSelectCity}: { city: City, keyword: string, onSelectCity: CityCallBack }) => {
+export const CitySuggestion = ({city, keyword, selected, onSelectCity}: { city: City, keyword: string, selected: boolean, onSelectCity: CityCallBack }) => {
     return (
         <button type="button"
-                className="list-group-item list-group-item-action list-group-item-light"
+                className={"list-group-item list-group-item-action list-group-item-light"}
+                style={{backgroundColor: selected ? "#e9ecef" : "white"}}
                 key={city.zip + city.name}
                 onClick={() => onSelectCity(city)}>
             <CityData city={city} keyword={keyword}/>

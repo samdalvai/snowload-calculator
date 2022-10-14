794c91c8960fd064a4f8bde327d3d197dece9696
samdalvai <samuel.dalvai@gmail.com>
2022-10-14 16:29:24 +0200

    Add margin to title card

--- backend/src/main/java/org/snowstop/repository/RetainerResistanceRepository.java
+++
@@ -1,7 +0,0 @@
-package org.snowstop.repository;
-
-import org.snowstop.model.RetainerResistance;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RetainerResistanceRepository extends JpaRepository<RetainerResistance, String> {
-}
--- frontend/src/components/card/TitleCard.tsx
+++ frontend/src/components/card/TitleCard.tsx
@@ -2,7 +2,8 @@ import React from "react";
 
 export const TitleCard = ({title}: {title: string}) => {
     return (
-        <div className={"card all-border text-center shadow-sm rounded snowload-lightgrey-background"}>
+        <div className={"card all-border text-center shadow-sm rounded snowload-lightgrey-background"}
+        style={{margin: "0 10% 0 10%"}}>
             <h2 className={"snowload-blue bold p-1"}>
                 {
                     title

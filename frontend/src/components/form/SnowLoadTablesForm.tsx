9183ca07c9fe3ae04d92b1e931dcca5e4aee5d21
samdalvai <samuel.dalvai@gmail.com>
2022-10-24 12:38:39 +0200

    Create components for roof data and snowload calculation tables

--- frontend/src/components/form/SnowLoadTablesForm.tsx
+++ frontend/src/components/form/SnowLoadTablesForm.tsx
@@ -10,6 +10,8 @@ import {PrintButton} from "../button/PrintButton";
 import {BackButton} from "../button/BackButton";
 import {AheadButton} from "../button/AheadButton";
 import {Title} from "../text/Title";
+import {RoofDatatable} from "../table/RoofDatatable";
+import {SnowLoadCalculationTable} from "../table/SnowLoadCalculationTable";
 
 export const SnowLoadTablesForm = ({roofData, snowLoadData, error, loading, onBack, onAhead}:
                                      { roofData: RoofData | null, snowLoadData: SnowLoadData, error: Boolean, loading: boolean, onBack: Callback, onAhead: Callback }) => {
@@ -28,81 +30,12 @@ export const SnowLoadTablesForm = ({roofData, snowLoadData, error, loading, onBa
                     !error ?
                         <div className={"row"}>
                             <div className="col-md-6 pt-3">
-                                <table className="table shadow-sm rounded">
-                                    <thead>
-                                    <tr className="table-secondary text-center shadow-sm table-header">
-                                        <th scope="row" colSpan={2}>{translation.tables.roofData.title}</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr>
-                                        <td width={"50%"}>{translation.tables.roofData.body.city}</td>
-                                        <td width={"50%"}>{roofData?.city.zip} {roofData?.city.name} ({roofData?.city.province})</td>
-                                    </tr>
-                                    <tr>
-                                        <td width={"50%"}>{translation.tables.roofData.body.steepness.label}</td>
-                                        <td width={"50%"}>{roofData?.steepness} °</td>
-                                    </tr>
-                                    <tr>
-                                        <td width={"50%"}>{translation.tables.roofData.body.roofLength.label}</td>
-                                        <td width={"50%"}>{roofData?.roofLength} m</td>
-                                    </tr>
-                                    <tr>
-                                        <td width={"50%"}>{translation.tables.roofData.body.roofWidth.label}</td>
-                                        <td width={"50%"}>{roofData?.roofWidth} m</td>
-                                    </tr>
-                                    <tr>
-                                        <td width={"50%"}>{translation.tables.roofData.body.safetyCoefficient.label}</td>
-                                        <td width={"50%"}>{roofData?.coefficient ? translation.words.yes : translation.words.no}</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
+                                <RoofDatatable  roofData={roofData}/>
                             </div>
                             <div className="col-md-6 pt-3">
-                                <table className="table shadow-sm rounded">
-                                    <thead>
-                                    <tr className="table-secondary text-center shadow-sm table-header">
-                                        <th scope="row" colSpan={2}>{translation.tables.snowLoadCalculation.title}</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr>
-                                        <td width={"50%"}>{translation.tables.snowLoadCalculation.body.altitude.label}</td>
-                                        <td width={"50%"}>{roofData?.city.altitude} {translation.tables.snowLoadCalculation.body.altitude.units}</td>
-                                    </tr>
-                                    <tr>
-                                        <td width={"50%"}>Climatic zone</td>
-                                        <td width={"50%"}>{snowLoadData.zone}</td>
-                                    </tr>
-                                    {
-                                        !loading ?
-                                            <>
-                                                <tr>
-                                                    <td width={"50%"}>{translation.tables.snowLoadCalculation.body.groundLoad.label}</td>
-                                                    <td width={"50%"}>{snowLoadData.groundLoad.toFixed(2)} kN/m<sup>2</sup>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td width={"50%"}>{translation.tables.snowLoadCalculation.body.roofLoad.label}</td>
-                                                    <td width={"50%"}>{snowLoadData.roofLoad.toFixed(2)} kN/m<sup>2</sup>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td width={"50%"}>{translation.tables.snowLoadCalculation.body.linearLoad.label}</td>
-                                                    <td width={"50%"}>{snowLoadData.linearLoad.toFixed(2)} kN/m</td>
-                                                </tr>
-                                            </>
-                                            :
-                                            <>
-                                                <tr>
-                                                    <td colSpan={2}>
-                                                        <DisabledInput placeHolder={translation.loading.computation}/>
-                                                    </td>
-                                                </tr>
-                                            </>
-                                    }
-                                    </tbody>
-                                </table>
+                                <SnowLoadCalculationTable  loading={loading}
+                                                           roofData={roofData}
+                                                           snowLoadData={snowLoadData}/>
                             </div>
                         </div>
                         :
---
+++ frontend/src/components/table/RoofDatatable.tsx
@@ -0,0 +1,41 @@
+import React, {useContext} from "react";
+import {LanguageContext} from "../language/LanguageContext";
+import {RoofData, SnowLoadData} from "../../functions/types";
+
+export const RoofDatatable = ({roofData}: { roofData: RoofData | null}) => {
+    const {translation} = useContext(LanguageContext);
+
+    return (
+        <div>
+            <table className="table shadow-sm rounded">
+                <thead>
+                <tr className="table-secondary text-center shadow-sm table-header">
+                    <th scope="row" colSpan={2}>{translation.tables.roofData.title}</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td width={"50%"}>{translation.tables.roofData.body.city}</td>
+                    <td width={"50%"}>{roofData?.city.zip} {roofData?.city.name} ({roofData?.city.province})</td>
+                </tr>
+                <tr>
+                    <td width={"50%"}>{translation.tables.roofData.body.steepness.label}</td>
+                    <td width={"50%"}>{roofData?.steepness} °</td>
+                </tr>
+                <tr>
+                    <td width={"50%"}>{translation.tables.roofData.body.roofLength.label}</td>
+                    <td width={"50%"}>{roofData?.roofLength} m</td>
+                </tr>
+                <tr>
+                    <td width={"50%"}>{translation.tables.roofData.body.roofWidth.label}</td>
+                    <td width={"50%"}>{roofData?.roofWidth} m</td>
+                </tr>
+                <tr>
+                    <td width={"50%"}>{translation.tables.roofData.body.safetyCoefficient.label}</td>
+                    <td width={"50%"}>{roofData?.coefficient ? translation.words.yes : translation.words.no}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
